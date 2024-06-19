e6f6e2c0df8e57470a98287a283a72fff55eef34
seb <155621580+seb-lecampus@users.noreply.github.com>
2024-06-19 11:38:20 +0200

    setup the gallery page

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,19 @@ use Symfony\Component\Routing\Attribute\Route;
 class WelcomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
-        return $this->render('layout/main.html.twig', [
-            'title' => 'WELCOME',
+        $Posts = $postRepository->findAll();
+        $Games = $postRepository->getAllDistinctGameName();
+        dump($Posts);
+        dump($Games);
+        return $this->render('Page/Gallery.html.twig', [
+            'posts' => array_map(function($post){
+                return (object)array('img' => "src", 'user_name' => $post->getUserName(), 'game' => $post->getGame());
+            }, $Posts),
+            'games' => array_map(function($game){
+                return $game["game"];
+            }, $Games)
         ]);
     }
 }
